a58c614403c8030eec3842f119178c7d68664d21
Daniel <dev.danielcw@gmail.com>
2024-07-09 13:42:54 -0500

    UPDATE(css): add stylesheet and static files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 
 from django.db.models import F
+from django.utils import timezone
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
