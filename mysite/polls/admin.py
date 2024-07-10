a76c475ed9b171b71581466b13205cb434f1ffc9
Daniel <dev.danielcw@gmail.com>
2024-07-09 20:48:05 -0500

    STYLE(poll): homepage styles, admin updates

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,16 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
